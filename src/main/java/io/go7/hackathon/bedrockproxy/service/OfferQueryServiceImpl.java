f18c48ee63723b1d48e8e63a22da3d154010aea5
Andrii Denysenko <adenysenko@go7.io>
2024-09-17 11:45:54 +0300

    add hints

--- src/main/java/io/go7/hackathon/bedrockproxy/service/OfferQueryServiceImpl.java
+++ src/main/java/io/go7/hackathon/bedrockproxy/service/OfferQueryServiceImpl.java
@@ -20,7 +20,7 @@ public class OfferQueryServiceImpl implements OfferQueryService {
 
     public static final String MODEL_ID = "anthropic.claude-3-sonnet-20240229-v1:0";
     public static final String WRAPPER =
-            "Get from the text IATA airport codes of the cities and dates and return only json as an example { departure: departure, arrival:arrival, departure_date: departure date} Text: %s ";
+            "Get from the text IATA airport codes of the cities and dates or approximate dates and return only json as an example { departure: departure, arrival:arrival, departure_date: departure date} Text: %s ";
 
     @Override
     public OfferQueryResponse getOfferQueryResponse(String query) {
@@ -39,29 +39,88 @@ public class OfferQueryServiceImpl implements OfferQueryService {
 
             var offerQueryResponse = new OfferQueryResponse();
 
-            if (map.containsKey("departure")) {
-                offerQueryResponse.setDeparture(AirportCode.of(map.get("departure").toString()));
+            fillDeparture(map, offerQueryResponse);
+
+            fillArrival(map, offerQueryResponse);
+
+            fillDepartureDate(map, offerQueryResponse);
+
+            if (offerQueryResponse.getPassengers().isEmpty()) {
+                offerQueryResponse.getPassengers().add(new PassengerQuantity(PassengerTypeGroup.ADULT, 1));
             }
 
-            if (map.containsKey("arrival")) {
-                offerQueryResponse.getArrival().add(AirportCode.of(map.get("arrival").toString()));
+            if (mandatoryDataFilled(offerQueryResponse)) {
+                offerQueryResponse.setFinalResult(true);
+            } else {
+                generateHint(offerQueryResponse);
             }
-
-            if (map.containsKey("departure_date")) {
-                offerQueryResponse.setDepartureDate(LocalDate.parse(map.get("departure_date").toString()));
-            }
-
-            offerQueryResponse.getPassengers().add(new PassengerQuantity(PassengerTypeGroup.ADULT, 1));
-            offerQueryResponse.setFinalResult(true);
             return offerQueryResponse;
 
         } catch (JsonProcessingException e) {
             log.error(e.getMessage());
             OfferQueryResponse offerQueryResponse = new OfferQueryResponse();
             offerQueryResponse.setFinalResult(false);
-            offerQueryResponse.setRequestHint("try another request");
+            offerQueryResponse.setRequestHint("Please, try another request");
             return offerQueryResponse;
         }
 
+    }
+
+    private static void fillDepartureDate(Map<String, Object> map, OfferQueryResponse offerQueryResponse) {
+        try {
+            if (map.containsKey("departure_date")) {
+                offerQueryResponse.setDepartureDate(LocalDate.parse(map.get("departure_date").toString()));
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private static void fillArrival(Map<String, Object> map, OfferQueryResponse offerQueryResponse) {
+        try {
+            if (map.containsKey("arrival")) {
+                offerQueryResponse.getArrival().add(AirportCode.of(map.get("arrival").toString()));
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private static void fillDeparture(Map<String, Object> map, OfferQueryResponse offerQueryResponse) {
+        try {
+            if (map.containsKey("departure")) {
+                offerQueryResponse.setDeparture(AirportCode.of(map.get("departure").toString()));
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private void generateHint(OfferQueryResponse offerQueryResponse) {
+        if (offerQueryResponse.getDeparture() == null
+                && offerQueryResponse.getArrival().isEmpty()
+                && offerQueryResponse.getDepartureDate() == null) {
+            offerQueryResponse.setRequestHint("Please provide information about where your departure and arrival places and date for your trip");
+        } else if (offerQueryResponse.getDeparture() == null && offerQueryResponse.getArrival().isEmpty()) {
+            offerQueryResponse.setRequestHint("Please provide information about where your departure and arrival places are");
+        } else if (offerQueryResponse.getDepartureDate() == null && offerQueryResponse.getArrival().isEmpty()) {
+            offerQueryResponse.setRequestHint("Please provide information about where your arrival place and trip date");
+        } else if (offerQueryResponse.getDepartureDate() == null && offerQueryResponse.getDepartureDate() == null) {
+            offerQueryResponse.setRequestHint("Please provide information about where your departure and trip date");
+        } else if (offerQueryResponse.getDeparture() == null) {
+            offerQueryResponse.setRequestHint("Please provide information about where your departure place");
+        } else if (offerQueryResponse.getArrival().isEmpty()) {
+            offerQueryResponse.setRequestHint("Please provide information about where your arrival place");
+        } else if (offerQueryResponse.getDepartureDate() == null) {
+            offerQueryResponse.setRequestHint("Please provide information about where your trip date");
+        }
+
+    }
+
+    private boolean mandatoryDataFilled(OfferQueryResponse offerQueryResponse) {
+        return !offerQueryResponse.getPassengers().isEmpty()
+                && offerQueryResponse.getDeparture() != null
+                && ! offerQueryResponse.getArrival().isEmpty()
+                && offerQueryResponse.getDepartureDate() != null;
     }
 }
